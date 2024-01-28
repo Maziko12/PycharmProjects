23ce505b7ccb09dbf7a23bed068e023e136e7ee9
LeoNardo <henriquecenoviczleo@gmail.com>
2024-01-27 21:39:11 -0300

    Exercício 48 (contar números divisíveis em soma por 3 ímpares do 1 ao 500), exercício 49 (Taboada utilizando for).

---
+++ PythonExercicios/ex048.py
@@ -0,0 +1,9 @@
+soma = 0
+for c in range(1, 500, 2):
+    if c % 3 == 0:
+        soma += c
+print(f'A soma dos números ímpares múltiplocs de três, dos números\n1 até 500 é de {soma}.')
+print('\nFim')
+
+# Faça um programa que calcule a soma entre todos os números ímpares que
+# se encontram no intervalo de 1 até 500.
